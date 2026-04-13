bb94d5d637608ebf65a921b5e327a1ae4bc324b2
RMalinowski <r.malinowski3211@gmail.com>
2026-04-13 23:16:55 +0200

    feat: add temporary 500 error message

--- src/components/views/errors/Error500.tsx
+++ src/components/views/errors/Error500.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import ErrorPageTemplate from "./ErrorPageTemplate";
 
 export default function Error404() {
@@ -5,6 +6,17 @@ export default function Error404() {
     <ErrorPageTemplate
       errorCode={500}
       errorMessage="Błąd wewnętrzny serwera. Przepraszamy"
-    />
+    >
+      <Typography variant="subtitle1" >
+        Ta aplikacja jest częścią mojego portfolio i backend nie został jeszcze wdrożony na serwerze.
+      </Typography>
+      <Typography variant="subtitle1" >
+        👉 Wersja lokalna działa w pełni - w razie potrzeby chętnie udostępnię instrukcję uruchomienia.
+      </Typography>
+      <Typography variant="subtitle1" sx={{ mb: "25px" }}>
+        Dziękuję za zainteresowanie projektem!
+      </Typography>
+
+    </ErrorPageTemplate>
   );
 }
